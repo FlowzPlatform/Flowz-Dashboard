1c0e140963c0022ed89811be17180f13f77e548a
avasani <avasani@officebrain.com>
2017-12-01 12:07:45 +0530

    fb and google login params taken from config file

--- config/customConfig.js
+++ config/customConfig.js
@@ -2,9 +2,9 @@ export default{
     loginUrl : "http://ec2-54-88-11-110.compute-1.amazonaws.com/api/login",
     registrationUrl : "http://api.flowz.com/serverapi/register",
     feathersServiceBaseUrl :  "http://api.flowz.com/serverapi/",
-    facebookSuccessCallbackUrl : "http://localhost:8081",
-    loginWithFacebookUrl : "http://ec2-54-88-11-110.compute-1.amazonaws.com/auth/facebook/callback",
-    facebookKey : "108343389875876",
-    facebookToken : "a121fc6c5188e589ef5b662866da47fe"
-
+    facebookSuccessCallbackUrl : "http://dashboard.flowz.com",
+    loginWithFacebookUrl : "http://ec2-54-88-11-110.compute-1.amazonaws.com/auth/facebook",
+    googleSuccessCallbackUrl : "http://dashboard.flowz.com",
+    loginWithGoogleUrl : "http://ec2-54-88-11-110.compute-1.amazonaws.com/auth/Gplus",
+    
 }
