de6c81df78d3caad62738b529947252a55df53cd
bhumilsarvaiya <bhumilsarvaiya@gmail.com>
2018-01-18 15:45:46 +0530

    config changes

--- src/config/index.js
+++ src/config/index.js
@@ -1,7 +1,52 @@
 console.log('process.env.NODE_ENV', process.env.NODE_ENV)
-var temp = {
-  serverURI: 'http://localhost:3030',
-  socketURI: 'http://localhost:3030',
-  microURI: 'http://auth.flowz.com/api',
+
+let domainkey = process.env.domainkey ? process.env.domainkey : 'flowzcluster.tk'
+let serverPort = process.env.server_port ? process.env.server_port : '3039'
+let socketPort = process.env.socket_port ? process.env.socket_port : '4039'
+let isSSl = process.env.is_ssl ? process.env.is_ssl : false
+let protocol = ''
+
+if (isSSl === 'true') {
+  protocol = 'https'
+} else {
+  protocol = 'http'
 }
+
+var temp = {
+  serverURI: protocol + '://localhost:' + serverPort,
+  socketURI: protocol + '://localhost:' + socketPort,
+  fixedLayout: false,
+  hideLogoOnMobile: false,
+  microURI: protocol + '://auth.' + domainkey + '/api',
+  facebookSuccessCallbackUrl: protocol + '://localhost:8000',
+  loginWithFacebookUrl: protocol + '://auth.' + domainkey + '/auth/facebook',
+  googleSuccessCallbackUrl: protocol + '://localhost:8000',
+  loginWithGoogleUrl: protocol + '://auth.' + domainkey + '/auth/Gplus',
+  twitterSuccessCallbackUrl: protocol + '://localhost:8000',
+  loginWithTwitterUrl: protocol + '://auth.' + domainkey + '/auth/twitter',
+  linkedinSuccessCallbackUrl: protocol + '://localhost:8000',
+  loginWithLinkedinUrl: protocol + '://auth.' + domainkey + '/auth/linkedin',
+  githubSuccessCallbackUrl: protocol + '://localhost:8000',
+  loginWithGithubUrl: protocol + '://auth.' + domainkey + '/auth/github',
+  jobQueueUrl: protocol + '://api.' + domainkey + '/rjobqueue1/job/create'
+}
+if (process.env.NODE_ENV !== 'development') {
+  temp = {
+    serverURI: protocol + '://api.' + domainkey + '/uploader', // 'http://api.' + process.env.domainkey + '/dbetl', // ws.' + process.env.domainkey + ':4034
+    socketURI: 'ws://ws.' + domainkey + ':' + socketPort,
+    fixedLayout: false,
+    hideLogoOnMobile: false,
+    microURI: protocol + '://auth.' + domainkey + '/api',
+    facebookSuccessCallbackUrl: protocol + '://uploader.' + domainkey + '/',
+    loginWithFacebookUrl: protocol + '://auth.' + domainkey + '/auth/facebook',
+    googleSuccessCallbackUrl: protocol + '://uploader.' + domainkey + '/',
+    loginWithGoogleUrl: protocol + '://auth.' + domainkey + '/auth/Gplus',
+    linkedinSuccessCallbackUrl: protocol + '://uploader.' + domainkey + '/',
+    loginWithLinkedinUrl: protocol + '://auth.' + domainkey + '/auth/linkedin',
+    githubSuccessCallbackUrl: protocol + '://uploader.' + domainkey + '/',
+    loginWithGithubUrl: protocol + '://auth.' + domainkey + '/auth/github',
+    jobQueueUrl: protocol + '://api.' + domainkey + '/rjobqueue1/job/create'
+  }
+}
+
 export default temp
