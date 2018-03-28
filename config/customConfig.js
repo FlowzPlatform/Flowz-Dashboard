d4a29280de00366a720f28bea986700601cc662a
chirag <chirag@officebrain.com>
2018-03-28 15:38:21 +0530

    update user api urls

--- config/customConfig.js
+++ config/customConfig.js
@@ -5,14 +5,14 @@ let frontEndUrl = 'https://dashboard.'+process.env.domainkey+'/';
 export default{
     // loginUrl : "http://ec2-54-88-11-110.compute-1.amazonaws.com/api/login",
     // ldapLoginUrl : "http://ec2-54-88-11-110.compute-1.amazonaws.com/api/ldapauth",
-    loginUrl: "https://auth."+process.env.domainkey+"/api/login",
+    loginUrl: "https://api."+process.env.domainkey+"/auth/api/login",
     ldapLoginUrl: "https://auth."+process.env.domainkey+"/api/ldapauth",
     // registrationUrl : "http://api."+process.env.domainkey+"/serverapi/register",
     registrationUrl : "https://auth."+process.env.domainkey+"/api/dashboardpass",
     feathersServiceBaseUrl :  "https://api."+process.env.domainkey+"/serverapi/",
     signupUrl : "https://api."+process.env.domainkey+"/auth/api/setup",
-    
-   
+
+
     facebookSuccessCallbackUrl: frontEndUrl,
     googleSuccessCallbackUrl: frontEndUrl,
     twitterSuccessCallbackUrl: frontEndUrl,
@@ -20,7 +20,7 @@ export default{
     linkedInSuccessCallbackUrl: frontEndUrl,
 
 
-    
+
 
         loginWithFacebookUrl : 'https://auth.'+process.env.domainkey+'/auth/facebook',
         loginWithGoogleUrl : 'https://auth.'+process.env.domainkey+'/auth/Gplus',
@@ -28,7 +28,7 @@ export default{
         loginWithGithubUrl : 'https://auth.'+process.env.domainkey+'/auth/github',
         loginWithLinkedInUrl : 'https://auth.'+process.env.domainkey+'/auth/linkedin',
 
-    
+
     userDetail:             "https://api."+process.env.domainkey+"/auth/api/userdetails",
     varifyEmailUrl :         "https://api."+process.env.domainkey+"/auth/api/verifyemail",
     forgotPasswordUrl :         "https://api."+process.env.domainkey+"/auth/api/forgetpassword",
