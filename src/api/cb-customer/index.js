57b5ae53824ecf9055f3bc654185fe08828e2329
Sanjaysinh Zala <sanjaysinhz@officebrain.com>
2018-05-23 20:19:52 +0530

    subscribe addon, plan and update my plan page

---
+++ src/api/cb-customer/index.js
@@ -0,0 +1,21 @@
+import api from '../../api';
+let model = 'cb-customer';
+
+export default {
+    get: (id) => {
+        if (id != undefined) {
+            return api.request('get', '/' + model + '/' + id);
+        } else {
+            return api.request('get', '/' + model);
+        }
+    },
+    post: (data) => {
+        return api.request('post', '/' + model, data);
+    }
+    /* put: (id, data) => {
+        return api.request('put', '/' + model + '/' + id, data);
+    },
+    delete: (id) => {
+        return api.request('delete', '/' + model + '/' + id);
+    } */
+}
