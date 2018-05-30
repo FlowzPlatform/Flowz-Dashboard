eeb3ba8d0d638b0ef0c98b955ad9adc1c8e5de47
Sanjaysinh Zala <sanjaysinhz@officebrain.com>
2018-05-30 17:41:08 +0530

    Done cb subscription

--- src/config/index.js
+++ src/config/index.js
@@ -1,7 +1,7 @@
 
 export default {
-  serverURI: 'http://localhost:3030',
-  // serverURI: 'https://api.' + process.env.domainkey + '/subscription',
+  // serverURI: 'http://localhost:3030',
+  serverURI: 'https://api.' + process.env.domainkey + '/subscription',
   // socketURI: 'http://localhost:3030',
   microURI: 'https://auth.' + process.env.domainkey + '/api',
   userDetailsAPI: 'https://api.' + process.env.domainkey + '/auth/api/',
