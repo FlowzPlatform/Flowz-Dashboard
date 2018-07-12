32b367f926e42ce85015ca4f61a373c374bcb1d0
Sanjaysinh Zala <sanjaysinhz@officebrain.com>
2018-07-12 16:19:22 +0530

    solve issue for wrong details of addon price and form autocomplete

--- src/components/add-on.vue
+++ src/components/add-on.vue
@@ -120,11 +120,7 @@ export default {
 					title: 'Total',
 					align: 'center',
 					render: (h, params) => {
-						let val
-						val = (params.row.price * params.row.quantity)
-						total += val
-						this.totalAddonPrice = total
-						this.$emit('totalAddon', total)
+						this.emitTotalPrice(params)
 						return h('span', params.row.price * params.row.quantity)
 					}
 				}
@@ -133,11 +129,22 @@ export default {
 			loading: true,
 			addOnListData: [],
 			addOnList: [],
+			counter: 0,
 			currentPage: 1,
 			pageSize: 10
 		}
 	},
 	methods: {
+		emitTotalPrice (params) {
+			let val
+			val = (params.row.price * params.row.quantity)
+			total += val
+			this.totalAddonPrice = total
+			if (params.index >= this.addOnList.length - 1) {
+				this.$emit('totalAddon', total)
+				total = 0
+			}
+		},
 		async changePage (pageNo) {
 			this.addOnList = await this.makeChunk(pageNo, this.pageSize)
 		},
@@ -152,7 +159,6 @@ export default {
 		},
 		getAddonDetails (id) {
 			return cbAddon.get(id).then(res => {
-				console.log('res >>>>>>>>>>>>>', res)
 				return res.data
 			})
 		}
